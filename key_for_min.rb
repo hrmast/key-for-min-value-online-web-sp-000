0981aebc998773e434c8b238ef831548e9ef67d5
Heath Mast <hrmast@gmail.com>
2020-05-04 03:40:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,21 +3,21 @@
 
 def key_for_min_value(name_hash)
 
-  current_minimum_val = nil
-  current_key_assoc_w_min_val = nil
+  minimum_val = nil
+  key_min_val = nil
 
   name_hash.each do |key, value|
 
-    if current_minimum_val == nil
-      current_minimum_val = value
-      current_key_assoc_w_min_val = key
+    if minimum_val == nil
+      minimum_val = value
+      key_min_val = key
     else
 
-      if current_minimum_val > value
-        current_minimum_val = value
-        current_key_assoc_w_min_val = key
+      if minimum_val > value
+        minimum_val = value
+        key_min_val = key
       end
     end
   end
-  current_key_assoc_w_min_val
+  key_min_val
 end
