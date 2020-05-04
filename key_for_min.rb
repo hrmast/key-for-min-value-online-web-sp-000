f11486be055ee3e2c218b5fc38e84fa9d15f8e7a
Heath Mast <hrmast@gmail.com>
2020-05-04 03:24:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  max_value = 0
   name_hash.collect do |key, value|
-    if value  
+    if value > max_value
       return key
+    else
+      return nil
   end
 end
